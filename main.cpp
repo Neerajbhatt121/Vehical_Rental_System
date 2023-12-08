dbebfe4f4cdffba518ae60ab4e68678c67b1e935
NeerajBhatt121 <neerajbhatt1912@gmail.com>
2023-12-08 05:49:01 -0800

    New changes

--- main.cpp
+++ main.cpp
@@ -1,62 +1,62 @@
-#include"other.h"
-#include"user.h"
-//#include"admin.h"
+#include "other.h"
+#include "user.h"
+// #include"admin.h"
 using namespace std;
 
-
-int main(){
+int main()
+{
     userclass uc;
     admin ad;
-intro();
-    // Refresh
-    Refresh:
-showmenu();
-int ch;
-cin>>ch;
-switch (ch)
-{
-case 1:
-    system("CLS");
-    headline2();
-   uc.userLogin();
-   system("PAUSE");
-  
-    break;
+    intro();
+// Refresh
+Refresh:
+    showmenu();
+    int ch;
+    cin >> ch;
+    switch (ch)
+    {
+    case 1:
+        system("CLS");
+        headline2();
+        uc.userLogin();
+        system("CLS");
+        goto Refresh;
+        break;
 
-case 2:
-    headline1();
-   ad.adminlogin();
-    break;
+    case 2:
+        headline1();
+        ad.adminlogin();
+        break;
 
-case 3:
-    system("CLS");
-    uc.showCarsFromFile();
-    uc.car_detail();
-    break;  
+    case 3:
+        system("CLS");
+        uc.showCarsFromFile();
+        uc.car_detail();
+        uc.book();
+        break;
 
-case 4:
-    uc.Search();
-    break;
+    case 4:
+        uc.Search();
+        break;
 
-case 5:
-    uc.createAcc();
-    break;
+    case 5:
+        uc.createAcc();
+        break;
 
-case 6:
-    system("main.exe");
-    break;
+    case 6:
+        system("main.exe");
+        break;
 
-case 7:
-    uc.booking();
-    break;
+    case 7:
+        uc.book();
+        break;
 
-default:
-    cout <<RED<<"==================================================" << endl; 
-    cout <<RED<<"         <<--SELECT CORRECT OPTION-->>" << RESET << endl;
-    cout<<RED<<"==================================================" << endl;
-  system("pause");
-    system("main.exe");
-    break;
-}
-
+    default:
+        cout << RED << "==================================================" << endl;
+        cout << RED << "         <<--SELECT CORRECT OPTION-->>" << RESET << endl;
+        cout << RED << "==================================================" << endl;
+        system("pause");
+        system("main.exe");
+        break;
+    }
 }
--- user.h
+++ user.h
@@ -1,200 +1,270 @@
 #ifndef USER_H
 #define USER_H
-#include<iostream>
+#include <iostream>
 #define GREEN "\033[32m"
-#include"admin.h"
-#include<fstream>
+#include "admin.h"
+#include <fstream>
+#include <cstdlib> // Include the necessary header for rand()
+#include <ctime>   // Include the necessary header for srand() and time()
 using namespace std;
 
-class userAcc{
-    public:
+class userAcc
+{
+public:
     ofstream userAdd;
     string username, userID, userPASS;
     vector<string> Username;
     vector<string> Password;
     vector<string> Userid;
-
 };
 
-class userclass : public type, public userAcc{
-    public:
-
-
-void userLogin(){
+class userclass : public type, public userAcc
+{
+public:
     string name, uname, pass;
-    cout<<"\t\tEnter The UserName:  "; cin>>name;
-    cout<<"\t\tEnter The PassWord:  "; cin>>pass;
-
-
+    int choice_index;
     ifstream sh;
-    sh.open("login.db" , ios::in);
-    string nAME ,uNAME, pASS;
-    bool found = false;
+    string comp, mdl, prc, spd;
 
-    while(sh >> nAME >> uNAME >> pASS){
-        if(uNAME == name  and  pASS == pass){
-        cout << "==================================================" << endl;
-    cout << GREEN << "          <<--LOGIN SUCCESSFULL-->>" << RESET << endl;
-        cout << "==================================================" << endl;
-            found = true;
-            break;
-        }
-       
+    // Function to generate a random booking number
+    int generateBookingNumber()
+    {
+        srand(time(nullptr));         // Seed the random number generator with the current time
+        return rand() % 10000 + 1000; // Generate a random number in the range [1000, 9999]
     }
-    if(!found){
-        cout << "==================================================" << endl;
-    cout << GREEN << "        <<--ACCOUNT NOT EXIST-->>" << RESET << endl;
-        cout << "==================================================" << endl;
-    }  
 
-}
+    void userLogin(){
+        cout << "\t\tEnter The UserName:  ";
+        cin >> uname;
+        cout << "\t\tEnter The PassWord:  ";
+        cin >> pass;
 
-void bookingCar(){
-    int choice;
-    cout<<"Do You Want To Book";
-    cout<<"\t1. YES \t2.NO";
-    cin>>choice;
-    if(choice!=1)
-    showCarsFromFile();
-
-    ifstream bk;
-    bk.open("booking.db", ios::app | ios::binary);
-    //bk.
-
-}
-
-
-//-------------------//
-void showCarsFromFile() {
         ifstream sh;
-        sh.open("storage.db", ios::in);
-            string comp, mdl, prc, spd;
-            int index = 0;
-            cout << YELLOW << "  |=============================================================================" << "|" << RESET << endl;
-            cout << YELLOW << "  |    \t" << BLUE << "COMPANY" << RESET << "\t\t|  " << BLUE << "PRICE  \t" << RESET << "| " << BLUE << "SPEED  \t" << RESET << "| " << BLUE << "MODEL \t\t\t" << RESET << "|" << endl;
-            cout << YELLOW << "  |=============================================================================" << "|" << RESET << endl;
-            while (sh >> comp >> mdl >> prc >> spd) {
-                
-                cout << YELLOW << "  |\t" << RESET << index+1 << " | " << comp << " \t| " << prc << " \t| " << spd << " \t| " << mdl << endl;
-                cout << YELLOW << "  |=============================================================================" << "|" << RESET << endl;
-                index++;
+        sh.open("login.db", ios::in);
+        string nAME, uNAME, pASS;
+        bool found = false;
+
+        while (sh >> nAME >> uNAME >> pASS)
+        {
+            if (uNAME == uname and pASS == pass)
+            {
+                cout << "==================================================" << endl;
+                cout << GREEN << "          <<--LOGIN SUCCESSFULL-->>" << RESET << endl;
+                cout << "==================================================" << endl;
+                found = true;
+                break;
             }
-            sh.close();
+        }
+        if (!found)
+        {
+            cout << RED << "==================================================" << endl;
+            cout << "        <<--ACCOUNT NOT EXIST-->>" << endl;
+            cout << "==================================================" << RESET << endl;
+        }
+
+        system("PAUSE");
         
     }
 
-void car_detail(){
-    int choice;  cout<<"\tEnter Your Choice:  ";
-    cin>>choice; choice=choice-1;
+    void booking(const string &loggedInUsername, const string &loggedInUserID, int &choice_index)
+    {
 
-    ifstream sh;
-    sh.open("storage.db" , ios::in);
-    if(!sh.is_open()) cout<<"\t\tError Opening File:";
+        int choice;
+        int index = 0;
+        cout << "Do You Want To Book?\n";
+        cout << "1. YES\t2. NO\n";
+        cin >> choice;
+        sh.open("storage.db", ios::in);
+        bool found = false;
 
-    string comp, mdl, prc, spd;
+        if (choice == 1)
+        {
+
+            while (sh >> comp >> mdl >> prc >> spd)
+            {
+                // Validate the selected car index
+                if (index == choice_index)
+                {
+                    // Generate a random booking number
+                    int bookingNumber = generateBookingNumber();
+
+                    // Open the booking database file
+                    ofstream bookingFile("booking.db", ios::app | ios::binary);
+
+                    // Write the booking details (username, userID, car details, and booking number) to the file
+                    bookingFile << loggedInUsername << "\n" << loggedInUserID << "\n"
+                                << comp << "\n" << bookingNumber << endl<<endl;
+
+                    // Close the booking file
+                    bookingFile.close();
+
+                    cout << "Booking successful! Your booking number is: " << bookingNumber << endl;
+                    found = true;
+                    break;
+                }
+                index++;
+            }
+        }
+        if (!found)
+        {
+            cout << "Invalid car index!\n";
+        }
+    }
+
+    //--------------------//
+
+    void showCarsFromFile()
+    {
+        ifstream sh;
+        sh.open("storage.db", ios::in);
+        string comp, mdl, prc, spd;
+        int index = 0;
+        cout << YELLOW << "  |============================================================================="
+             << "|" << RESET << endl;
+        cout << YELLOW << "  |    \t" << BLUE << "COMPANY" << RESET << "\t\t|  " << BLUE << "PRICE  \t" << RESET << "| " << BLUE << "SPEED  \t" << RESET << "| " << BLUE << "MODEL \t\t\t" << RESET << "|" << endl;
+        cout << YELLOW << "  |============================================================================="
+             << "|" << RESET << endl;
+        while (sh >> comp >> mdl >> prc >> spd)
+        {
+
+            cout << YELLOW << "  |\t" << RESET << index + 1 << " | " << comp << " \t| " << prc << " \t| " << spd << " \t| " << mdl << endl;
+            cout << YELLOW << "  |============================================================================="
+                 << "|" << RESET << endl;
+            index++;
+        }
+        sh.close();
+    }
+
+    void car_detail()
+    {
+        cout << "\tEnter Your Choice:  ";
+        cin >> choice_index;
+        choice_index = choice_index - 1;
+
+        sh.open("storage.db", ios::in);
+        if (!sh.is_open())
+            cout << "\t\tError Opening File:";
+
         int index = 0;
         bool found = false;
 
-while(sh >> comp >> mdl >> prc >> spd){
-        if(index==choice){
-        cout<<"  |=====================================|\n";
-cout<<"  |"<<YELLOW<<" \tYOU HAVE SELECTED  : "<<comp<<RESET<<"\t|"<<endl;
-        cout<<"  |=====================================|\n";
-        cout<<"  |\tModel  :  "<<mdl<<"\t\t|"<<endl;
-        cout<<"  |\tPrice  :  "<<prc<<"\t\t|"<<endl;
-        cout<<"  |\tSpeed  :  "<<spd<<"\t\t\t|"<<endl;
-        cout<<"  |=====================================|";
-            found = true;
-            break;
+        while (sh >> comp >> mdl >> prc >> spd)
+        {
+            if (index == choice_index)
+            {
+                cout << "  |=====================================|\n";
+                cout << "  |" << YELLOW << " \tYOU HAVE SELECTED  : " << comp << RESET << "\t|" << endl;
+                cout << "  |=====================================|\n";
+                cout << "  |\tModel  :  " << mdl << "\t\t|" << endl;
+                cout << "  |\tPrice  :  " << prc << "\t\t|" << endl;
+                cout << "  |\tSpeed  :  " << spd << "\t\t\t|" << endl;
+                cout << "  |=====================================|";
+                found = true;
+                break;
+            }
+            index++;
         }
-        index++;
+        sh.close();
+        if (!found)
+        {
+            cout << RED << "\t\tInvalid choice!" << RESET << endl;
+        }
+        cout << endl;
     }
-    sh.close();
-     if (!found) {
-        cout << RED << "\t\tInvalid choice!" << RESET << endl;
-    }
-}
 
-//------------------//
+    //------------------//
 
-void Search() {
-    string name;
-    system("CLS");
-    cout << "SEARCH BY NAME \n";
-    cout << "Enter the name of the car: ";
-    cin >> name;
+    void Search()
+    {
+        string name;
+        system("CLS");
+        cout << "SEARCH BY NAME \n";
+        cout << "Enter the name of the car: ";
+        cin >> name;
 
-     ifstream sh;
+        ifstream sh;
         sh.open("storage.db", ios::in);
 
-    if (!sh.is_open()) {
-        cout << "Error opening file!" << endl;
-        return;
-    }
+        if (!sh.is_open())
+        {
+            cout << "Error opening file!" << endl;
+            return;
+        }
 
-    string comp, mdl, prc, spd;
-    bool found = false;
+        string comp, mdl, prc, spd;
+        bool found = false;
 
-    while (sh >> comp >> mdl >> prc >> spd) {
-        if (name == comp) {
-            cout << YELLOW << "  |=============================================================================" << "|" << RESET << endl;
-            cout << YELLOW << "  |\t" << RESET << company.size()+1 << " | " << comp << " \t| " << prc << " \t| " << spd << " \t| " << mdl << endl;
-            cout << YELLOW << "  |=============================================================================" << "|" << RESET << endl;
-            found = true;
-            break; // Assuming you want to stop searching after finding the first match
+        while (sh >> comp >> mdl >> prc >> spd)
+        {
+            if (name == comp)
+            {
+                cout << YELLOW << "  |============================================================================="
+                     << "|" << RESET << endl;
+                cout << YELLOW << "  |\t" << RESET << company.size() + 1 << " | " << comp << " \t| " << prc << " \t| " << spd << " \t| " << mdl << endl;
+                cout << YELLOW << "  |============================================================================="
+                     << "|" << RESET << endl;
+                found = true;
+                break; // Assuming you want to stop searching after finding the first match
+            }
+        }
+
+        if (!found)
+        {
+            cout << "==================================================" << endl;
+            cout << RED << " <<--THIS CAR IS NOT AVAILABLE-->>" << RESET << endl;
+            cout << "==================================================" << endl;
         }
     }
 
-    if (!found) {
-        cout << "==================================================" << endl;
-        cout << RED << " <<--THIS CAR IS NOT AVAILABLE-->>" << RESET << endl;
-        cout << "==================================================" << endl;
-    }
-}
+    //------------------//
 
-//------------------//
+    void adduserAcc()
+    {
+        userAdd.open("login.db", ios::in | ios::binary);
+        userAdd << "Username:       userID:         Password:" << endl;
+        userAdd.close();
 
-void adduserAcc(){
-     userAdd.open("login.db",  ios::in | ios::binary );
-    userAdd << "Username:       userID:         Password:"<<endl;
-    userAdd.close();
-
-    userAdd.open("login.db",  ios::in   | ios::app  | ios::binary );
-      for (size_t i = 0; i < Username.size(); i++) {
-userAdd<< Username[i] << "     "<<Userid[i]<<"       " << Password[i] << endl;
+        userAdd.open("login.db", ios::in | ios::app | ios::binary);
+        for (size_t i = 0; i < Username.size(); i++)
+        {
+            userAdd << Username[i] << "     " << Userid[i] << "       " << Password[i] << endl;
         }
-    userAdd.close();
-}
+        userAdd.close();
+        cout << endl;
+        cout << "  |=====================================|\n";
+        cout << "  |" << YELLOW << " \tACCOUNT CREATED SUCESSFULLY : " << RESET << "\t|" << endl;
+        cout << BLUE << "  |=====================================|\n"
+             << RESET;
+        system("PAUSE");
+        system("main.exe");
+    }
 
-void createAcc(){
-     
+    void createAcc()
+    {
+
         cout << "Enter Username: ";
-        getline(cin, username);
-        cout<<"Enter userID: ";
-        cin>>userID;
+        cin >> username;
+        cout << "Enter userID: ";
+        cin >> userID;
         cout << "Enter password: ";
-        cin >>userPASS;
+        cin >> userPASS;
         Username.push_back(username);
         Userid.push_back(userID);
         Password.push_back(userPASS);
-     
-     adduserAcc();
-}
 
-//------------------//
+        adduserAcc();
+    }
 
-void booking(){
-    showCarsFromFile();
-    car_detail();
-    system("PAUSE");
-}
+    //------------------//
 
-
-
-
-
-
+    void book()
+    {
+        system("CLS");
+        system("PAUSE");
+        showCarsFromFile();
+        car_detail();
+        booking(uname, pass, choice_index);
+    }
 };
-
 
 #endif
